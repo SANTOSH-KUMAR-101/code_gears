32e369d3c87bec329380453e750c2a33841e7237
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-11-27 23:42:07 +0530

    1.0

--- 04_for_loops.js
+++ 04_for_loops.js
@@ -1,1 +1,13 @@
 // (1) Use for in loop in self defined problem
+
+let obj = {
+  "name" : "Santosh"
+  ,"Class" : 12}
+
+for( var iter in obj){
+   console.log(iter);
+  if (iter == "name"){
+    console.log(obj[iter]);   // console.log(obj.iter); not valid form of property testing
+  } 
+  
+ }; //Iterates throught the property
