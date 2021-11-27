0fe7d786ee99b20ad7f7818d90cc345415843f5d
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-11-27 18:52:16 +0530

    regex_gear_2.3

--- regex_gear_02.js
+++ regex_gear_02.js
@@ -16,3 +16,13 @@ var regex = /^alpha/i   //here i is an optinal flag given to test the case insen
 const string = "This is alpha aplha aplha beta gamma sting lapalapalpha";
 
 console.log(regex.test(string));
+
+
+//(3) Create a Regex variable and test it using Regex.test that the string exits in end.
+
+//Solution:- 
+var regex = /alpha$/i   //here i is an optinal flag given to test the case insensitive regex //^ Flag is used for checking string at the begining
+
+const string = "This is alpha aplha aplha beta gamma sting lapalapalpha";
+
+console.log(regex.test(string));
