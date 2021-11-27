d16601e880352ff2f1751b6c90e125ed49ff7853
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-11-27 20:14:55 +0530

    3.2

--- 03_conditional_gear.js
+++ 03_conditional_gear.js
@@ -25,3 +25,28 @@ function toss(para = "no toss"){
 //   toss("tail");
 //    toss("no toss");
 //    toss("invalid");
+
+// (2) Cascading effect of Switch Block if no break statement
+function toss(para = "no toss"){
+  
+  switch(para){
+      
+    case "no toss":
+      console.log("let us toss the coin"); // If we have a matching statement then every other thing will be executed till,
+                                           // we hit a break statement
+      
+    
+   case "head":
+    console.log("Team India  won the toss");
+    break;
+      
+    case "tail":
+     console.log("Team Bangladesh won the toss");
+     break;
+      
+      
+    case "invalid":
+     console.log("let us toss the coin again");
+     break;}}
+      
+  toss("head");
