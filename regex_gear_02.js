4f31e09d1c24a303bce6c0f39db8842e0eb051b7
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-11-27 14:00:21 +0530

    regex_gear_2.1

--- regex_gear_02.js
+++ regex_gear_02.js
@@ -1,1 +1,8 @@
 //(1) Create a Regex variable and test it using Regex.test
+
+//Solution:- 
+var regex = /alpha/i   //here i is an optinal flag given to test the case insensitive regex
+
+const string = "This is alpha aplha aplha beta gamma sting lapalapalpha";
+
+console.log(regex.test(string));
