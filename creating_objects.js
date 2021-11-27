ddf18d84229ca7a79dc01f963050f60d95f2b9e6
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-11-27 19:30:45 +0530

    object_gears_1.0

--- creating_objects.js
+++ creating_objects.js
@@ -2,7 +2,6 @@
 
 // ........................Task Assigned......................................
 // Q1: Create a object with properties and method.
-
 // Q2: Delete the object and add properties to the object.
 // Q3: Create a constructor function to create an object.
 // Q4: Create a Object Using Class.
