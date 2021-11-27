8f36665264737293ac146dfb423f2964511b7587
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-11-28 00:49:28 +0530

    Update 05_functions.js

--- 05_functions.js
+++ 05_functions.js
@@ -1,1 +1,25 @@
+// (1.0) Create a function that takes any number of arguments and adds it up
 
+function addingMachine() {
+  var sum = 0;
+  var length = arguments.length;
+  console.log(`Argument length is ${length}`);
+  if (length == 0) {
+    console.log(`Present Sum is 0; Give the machine numbers to add`);
+    return;
+  }
+
+  for (let i = 0; length > i; i += 1) {
+    var number = arguments[i];
+    sum += number;
+  }
+  console.log(`${sum}`);
+}
+
+addingMachine(9, 11, 56);
+
+
+
+
+    
+    
