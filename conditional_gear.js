7f2cf25460d06d801c80cacd45172f3c660ba795
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-11-27 19:58:31 +0530

    conditional_gear_1.0

--- conditional_gear.js
+++ conditional_gear.js
@@ -1,1 +1,27 @@
-(1) Use a switch block to mark down the outcomes of the toss in a match
+// (1) Use a switch block to mark down the outcomes of the toss in a match
+
+function toss(para = "no toss"){
+  
+  switch(para){
+      
+    case "no toss":
+      console.log("let us toss the coin");
+      break;
+    
+   case "head":
+    console.log("Team India  won the toss");
+    break;
+      
+    case "tail":
+     console.log("Team Bangladesh won the toss");
+     break;
+      
+      
+    case "invalid":
+     console.log("let us toss the coin again");
+     break;}}
+      
+  toss("head");
+//   toss("tail");
+//    toss("no toss");
+//    toss("invalid");
