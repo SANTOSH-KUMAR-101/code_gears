a668649027e1540bbe8b6b839a422d4cd8259371
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-11-28 01:25:32 +0530

    1.0

--- 06_Scope_of_Variables.js
+++ 06_Scope_of_Variables.js
@@ -1,1 +1,13 @@
+//(1.0) Find the difference between Global Scope, Var Scope and let scope by using them at different places.
+var name = "outside_var"
 
+function name2(){
+  console.log(`inside function var name is ${name}`); // outside var name is not visible inside
+  var name = "inside_var";
+  console.log(`inside function var name is ${name}`); // var name is inside only after declaration
+return name;
+}
+
+console.log(name); //function var is not visible
+name2();
+  
