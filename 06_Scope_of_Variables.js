25d1434f1e3d12bb2b9c31a91420b4197136e883
SANTOSH KUMAR <santosk.kumar.101@gmail.com>
2021-11-28 11:58:03 +0530

    3.0

--- 06_Scope_of_Variables.js
+++ 06_Scope_of_Variables.js
@@ -1,19 +1,46 @@
+//(3.0) Find the difference between var Scope outside and inside block scope.
+
+var a = "Global Value";
+
+if(a== undefined){
+  console.log(`If statement says the value of a inside Block: ${a}`);
+  var a = "Local Scope";
+}
+else{
+  console.log(`Else statement says the value of a inside Block: ${a}`);
+var a = "Local Scope";}
+
+
+console.log(`The value of a outside Block: ${a}`);
+
+//var variables behave as a global scope inside the Block Scope and different in Function Scope
+
+
+
+
+
+
+
+
+
+
+
+
 //(2.0) Find the difference between Global Scope outside and inside function scope.
-name = "outside_var"
+name = "global_var";
 
 function name2(){
-  console.log(`inside function var name is ${name}`); // Now outside name is visible inside function scope
-  name = "inside_var";
-  console.log(`inside function var name is ${name}`);
+  console.log(`Inside function var name is ${name}`); // Now outside name is visible inside function scope
+  name = "local_var";
+  console.log(`After local insertion the inside function var name is ${name}`);
 return name;
 }
 
 
-name2(); 
-console.log(name); // Function name is visible outside
+console.log(`The value of var after function call: ${name2()}`); // Value of varible taken inside the function
+console.log(`The value of var ${name}`); // Function name is visible outside
 
-
-
+//The Global Var changes values and it is accessible inside function.
 
 
 
@@ -24,12 +51,14 @@ console.log(name); // Function name is visible outside
 var name = "outside_var"
 
 function name2(){
-  console.log(`inside function var name is ${name}`); // outside var name is not visible inside
+  console.log(`Inside function var name is ${name}`); // outside var name is not visible inside
   var name = "inside_var";
-  console.log(`inside function var name is ${name}`); // var name is inside only after declaration
+  console.log(`After local insertion the inside function var name is ${name}`); // var name is inside only after declaration
 return name;
 }
 
-console.log(name); //function var is not visible
-name2();
-  
+console.log(`The value of var after function call: ${name2()}`); // Value of varible taken inside the function
+console.log(`The value of var ${name}`);
+ // var name is not accessible inside the function scope
+// if you setup a var value inside function then output will be according to that varibale only
+// outside var value doesnot changes
